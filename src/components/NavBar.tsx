e01cebf04964f3481f7879fa913bb20a5f3d5097
Farabi Askar <askar.farabi12@gmail.com>
2022-06-24 17:00:07 +0600

    initial commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import { Button, Menu, Typography, Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
+type Props = {};
+
+export const NavBar = ({}: Props) => {
+  return <div>JCrypto</div>;
+};
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export const HomePage = (props: Props) => {
+  return <div>HomePage</div>;
+};
