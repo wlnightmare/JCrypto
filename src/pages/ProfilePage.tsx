999cd99fed0da73800fb847bbb1764137388c80b
Farabi Askar <askar.farabi12@gmail.com>
2022-07-02 00:32:56 +0600

    created profile page

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@mui/material";
+import { removeUser } from "../app/userSlice";
+import { useAppDispatch } from "../hooks/redux-hooks";
+import { useAuthUser } from "../hooks/useAuthUser";
+
+type Props = {};
+
+const ProfilePage = (props: Props) => {
+  const dispatch = useAppDispatch();
+  const { email } = useAuthUser();
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <Button onClick={() => dispatch(removeUser())}>
+        Log Out from {email}
+      </Button>
+    </div>
+  );
+};
+
+export default ProfilePage;
