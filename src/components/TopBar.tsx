1a4611a6a713d8742f1ede3b10c4f63b81e788e7
Farabi Askar <askar.farabi12@gmail.com>
2022-07-03 00:56:46 +0600

    login logout logic

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,26 +1,27 @@
-import React from "react";
 import Brightness3Icon from "@mui/icons-material/Brightness3";
 import { useLocation } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
 import { setOpen } from "../app/modalSlice";
 import { setDarkTheme, setDefaultTheme } from "../app/themeSlice";
 import { setCurrency } from "../app/symbolSlice";
 import { Button, Container, NativeSelect, styled } from "@mui/material";
 import { ModeType } from "../types";
 import { COLORS } from "../constants/color";
-import { RootState } from "../app/store";
-import RegisterModal from "./Modal";
+import { AuthModal } from "./AuthModal";
 import LightModeIcon from "@mui/icons-material/LightMode";
+import { useAppDispatch, useAppSelector } from "../hooks/redux-hooks";
+import { removeUser, setUser } from "../app/userSlice";
+import { auth, onAuthStateChanged } from "../firebase";
+import { useEffect } from "react";
 
 const StyledButton = styled(Button)`
-  position: fixed;
+  position: absolute;
   z-index: 1000;
   top: 10px;
   right: 3%;
   color: orange;
 `;
 const StyledSelect = styled(NativeSelect)<ModeType>`
-  position: fixed;
+  position: absolute;
   color: ${(props) =>
     props.mode ? `${COLORS.DETAILS}` : `${COLORS.SECONDARY}`};
   background-color: transparent;
@@ -42,7 +43,7 @@ const StyledSelect = styled(NativeSelect)<ModeType>`
   }
 `;
 const OpenModalButton = styled(Button)<ModeType>`
-  position: fixed;
+  position: absolute;
   border: ${(props) =>
     props.mode ? `${COLORS.DETAILS}` : `${COLORS.SECONDARY}`};
   color: ${(props) =>
@@ -60,12 +61,27 @@ const OpenModalButton = styled(Button)<ModeType>`
 
 const TopBar = () => {
   const pathname = useLocation().pathname;
-
-  const dispatch = useDispatch();
-  const mode = useSelector((state: RootState) => state.mode.darkmode);
-  const open = useSelector((state: RootState) => state.modal.open);
-  const currency = useSelector((state: RootState) => state.currency.currency);
-
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+  const mode = useAppSelector((state) => state.mode.darkmode);
+  const open = useAppSelector((state) => state.modal.open);
+  const currency = useAppSelector((state) => state.currency.currency);
+  useEffect(() => {
+    const unSubcribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        dispatch(
+          setUser({
+            email: userAuth.email,
+            id: userAuth.uid,
+            token: userAuth.refreshToken,
+          })
+        );
+      } else {
+        dispatch(removeUser());
+      }
+    });
+    return () => unSubcribe();
+  }, [auth]);
   const handleClick = () => {
     dispatch(setOpen());
   };
@@ -77,11 +93,20 @@ const TopBar = () => {
   const setDefault = () => {
     dispatch(setDefaultTheme());
   };
+  const handleLogOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(removeUser());
+      })
+      .catch((err) => alert(err.message));
+  };
   const handleChange = (event: any) => {
     if (typeof event.target.value === "string") {
       dispatch(setCurrency(event.target.value));
     }
   };
+
   return (
     <>
       {pathname !== "/news" ? (
@@ -95,10 +120,16 @@ const TopBar = () => {
         </StyledSelect>
       ) : null}
 
-      <OpenModalButton mode={mode} onClick={handleClick}>
-        Sign in
-      </OpenModalButton>
-      {open && <RegisterModal open={open} />}
+      {!user ? (
+        <OpenModalButton mode={mode} onClick={handleLogOut}>
+          Logout
+        </OpenModalButton>
+      ) : (
+        <OpenModalButton mode={mode} onClick={handleClick}>
+          <div>Login {open && <AuthModal open={open} />}</div>
+        </OpenModalButton>
+      )}
+
       {!mode ? (
         <StyledButton onClick={setDark}>
           <Brightness3Icon />
