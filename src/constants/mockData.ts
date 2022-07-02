8246b26634374236447b4b67e01166aaa82d019d
Farabi Askar <askar.farabi12@gmail.com>
2022-07-03 01:06:11 +0600

    added chart data

--- src/constants/mockData.ts
+++ src/constants/mockData.ts
@@ -5,3 +5,22 @@ export const titles = [
   { header: "Total Markets", key: "totalMarkets" },
   { header: "Total Market Cap", key: "totalMarketCap" },
 ];
+
+export const time = [
+  {
+    label: "24 Hours",
+    value: "24h",
+  },
+  {
+    label: "30 Days",
+    value: "30d",
+  },
+  {
+    label: "3 Months",
+    value: "3m",
+  },
+  {
+    label: "1 Year",
+    value: "1y",
+  },
+];
