90b0f7321f47ee3d291f1615bcdc9c82da1b1616
Tansuture <tansuturemuratova@gmail.com>
2022-07-03 19:22:26 +0600

    removed unnecessary const

--- src/constants/mockData.ts
+++ src/constants/mockData.ts
@@ -5,6 +5,3 @@ export const titles = [
   { header: "Total Markets", key: "totalMarkets" },
   { header: "Total Market Cap", key: "totalMarketCap" },
 ];
-
-export const backgroundUrl =
-  "https://assets.euromoneydigital.com/dims4/default/4f6fbce/2147483647/strip/true/crop/800x490+0+0/resize/840x515!/quality/90/?url=http%3A%2F%2Feuromoney-brightspot.s3.amazonaws.com%2Ffc%2F1c%2F8c6efc9a63666ed6bd5963e4b54f%2Fdigital.jpg";
