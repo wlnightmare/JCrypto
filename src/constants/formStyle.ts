9525c8134308db3b07b166f2d336252b2b449bfa
Farabi Askar <askar.farabi12@gmail.com>
2022-07-03 16:48:25 +0600

    exported form styles

--- src/constants/formStyle.ts
+++ src/constants/formStyle.ts
@@ -1,11 +1,36 @@
-export const modalStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import { Box, Button, styled } from "@mui/material";
+import { COLORS } from "./color";
+
+export const StyledBox = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 350px;
+  background-color: ${COLORS.WHITE};
+  border: 2px solid #000;
+  box-shadow: 24;
+  border-radius: 15px;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  @media (max-width: 400px) {
+    width: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+export const StyledLine = styled("span")`
+  text-align: center;
+  margin-left: 42px;
+`;
+export const CustomBox = styled("div")`
+  padding: 25px;
+`;
+
+export const StyledSubmitButton = styled(Button)`
+  display: block;
+  margin: 0 auto;
+  width: 220px;
+`;
