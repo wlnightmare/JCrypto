a8a60cacf0db425e72409e0f3b379e5ed7f803f5
Farabi Askar <askar.farabi12@gmail.com>
2022-07-03 16:48:09 +0600

    added hook for auth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,11 @@
+import { useAppSelector } from "./redux-hooks";
+
+export function useAuth() {
+  const { email, token, id } = useAppSelector((state) => state.user);
+  return {
+    isAuth: !!email,
+    email,
+    token,
+    id,
+  };
+}
