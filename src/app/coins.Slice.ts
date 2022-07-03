0901960f71f5b3106c4abcc8d0d57dc87e1c7c4c
Tansuture <tansuturemuratova@gmail.com>
2022-07-03 19:18:44 +0600

    created slice for coins

---
+++ src/app/coins.Slice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Currency } from "../types";
+
+export {};
+
+const initialState = {
+  coins:
+    JSON.parse(localStorage.getItem("coins") || "[]") || ([] as Currency[]),
+};
+
+const coinSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    setData(state, action) {
+      state.coins = action.payload;
+
+      const mapped = state?.coins.map((element: Currency) => ({
+        ...element,
+        done: false,
+      }));
+      state.coins = mapped;
+    },
+    toggledone(state, action) {
+      const index = state.coins.findIndex(
+        (item: Currency) => item.uuid === action.payload.uuid
+      );
+      state.coins[index].done = true;
+      localStorage.setItem("coins", JSON.stringify(state.coins));
+    },
+  },
+});
+export const { setData, toggledone } = coinSlice.actions;
+
+export default coinSlice.reducer;
