a9943531e97e9cc2962d9ec07424fe88782af664
Tansuture <tansuturemuratova@gmail.com>
2022-07-03 19:56:26 +0600

    added new colors

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -2,7 +2,7 @@ export enum COLORS {
   HEADER = "#FFBD1F",
   SECONDARY = "black",
   DETAILS = "#73706f",
-  WHITE = "#EDF5E1",
+  WHITE = "white",
   LIGHT = "#7251b2",
   TITLE = "#e5e4e2",
   MAIN = "#f5f5f5",
